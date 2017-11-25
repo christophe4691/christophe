587b838b3fbefc00223d5b2444066ffbdf423341
christophe <doignon.christophe@orange.fr>
2017-11-25 14:04:37 +0100

    ajout calculatrice

---
+++ calculatrice/app.js
@@ -0,0 +1,89 @@
+function zero() {
+var x= document.getElementById("zero").value;
+   console.log(x);
+   var valeur=document.getElementById("resultat").value;
+   document.getElementById("resultat").value = valeur + x;
+ }
+ function un() {
+ var x= document.getElementById("un").value;
+ 	console.log(x);
+ 	var valeur=document.getElementById("resultat").value;
+ 	document.getElementById("resultat").value = valeur + x; 
+ }
+ function deux() {
+ var x= document.getElementById("deux").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur + x;
+}
+ function trois() {
+ var x= document.getElementById("trois").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur + x;
+}    
+ function quatre() {
+ var x= document.getElementById("quatre").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur + x;
+ }
+ function cinq() {
+ var x= document.getElementById("cinq").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur + x;
+ }
+ function six() {
+ var x= document.getElementById("six").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur + x;
+ }
+ function sept() {
+ var x= document.getElementById("sept").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur + x;
+ }
+ function huit() {
+ var x= document.getElementById("huit").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+}
+    function neuf() {
+ var x= document.getElementById("neuf").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur + x;
+ }
+ function minus() {
+ var x= document.getElementById("minus").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur - x;
+ }
+ function multiplication() {
+ var x= document.getElementById("multiplication").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur * x;
+}
+function division() {
+ var x= document.getElementById("division").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur / x;
+}
+ function left() {
+ var x= document.getElementById("left").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur "(" x;
+}
+function right() {
+ var x= document.getElementById("right").value;
+    console.log(x);
+    var valeur=document.getElementById("resultat").value;
+    document.getElementById("resultat").value = valeur ")" x;      
+}
---
+++ calculatrice/js/app.js
@@ -0,0 +1,6 @@
+function zero() {
+var x= document.getElementById("zero").value;
+   console.log(x);
+   var valeur=document.getElementById("resultat").value;
+   document.getElementById("resultat").value = valeur + x;
+ }
